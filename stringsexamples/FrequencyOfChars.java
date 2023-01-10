5ae4762761b8d9b987666c98a17c7cd067469bf6
MalleshSalla <105362483+MalleshSalla@users.noreply.github.com>
2023-01-10 22:51:18 +0530

    added programs on string

---
+++ stringsexamples/FrequencyOfChars.java
@@ -0,0 +1,83 @@
+package com.corejava.stringsexamples;
+
+import java.util.Scanner;
+
+public class FrequencyOfChars{
+	public static void main(String[] args) {
+		
+		String str;
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Enter the statement");
+		
+		str = sc.nextLine();
+		int count=0,len =0;
+		do {
+			try {
+				//this loop will identify character and find how many times it occurs
+				char name[]= str.toCharArray();
+				len=name.length;
+				count=0;
+				for(int j=0;j<len;j++) {
+					if ((name[0]==name[j])&&((name[0]>=65&&name[0]<=91)||(name[0]>=97&&name[0]<=123))) {
+						count++;
+					}
+				}
+				if(count!=0) {
+					System.out.println(name[0]+" "+count+" Times");
+				}
+				str = str.replace("" + name[0],"");				
+			}
+			catch(Exception e)
+			{
+				
+			}
+		}
+		while(len!=0);
+		sc.close();
+		
+		main("salla malelsh");
+		
+	}
+	public static void main(String str) {
+		System.out.println("Enter a string value to chech frequency of charecters");
+		
+		int count = 0,len=0;
+		do {
+			try {
+				char name[] = str.toCharArray();
+				len = name.length;
+				count=0;
+				for (int i = 0; i < len; i++) {
+					if ( (name[0]==name[i]) && ( (name[0]>=65&&name[0]<=91)||(name[0]>=97&&name[0]<=123) ) ) {
+						count++;
+					}
+				}
+				
+				if (count!=0) {
+					System.out.println(name[0]+" "+count+" Times");
+				}
+				str =str.replace("" + name[0],"");
+				
+			} catch (Exception e) {
+				// TODO: handle exception
+			}
+			
+		} while (len!=0);
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
